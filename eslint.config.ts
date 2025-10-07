80396ef73247fafc20b79b5c99354da763790e6d
kh4f <kh4f.dev@gmail.com>
2025-10-07 07:17:32 +0300

    chore(eslint): add initial ESLint configuration
    Added `eslint.config.ts` with base rules, type-aware rules, and stylistic rules.

---
+++ eslint.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'eslint/config'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import globals from 'globals'
+
+export default defineConfig([
+	{
+		name: 'Base Rules',
+		files: ['**/*.ts'],
+		extends: [eslint.configs.recommended],
+		languageOptions: { globals: { ...globals.node } },
+	},
+	{
+		name: 'Type-Aware Rules',
+		files: ['**/*.ts'],
+		extends: [
+			tseslint.configs.strictTypeChecked,
+			tseslint.configs.stylisticTypeChecked,
+		],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		name: 'Stylistic Rules',
+		files: ['**/*.ts'],
+		extends: [stylistic.configs.recommended],
+		rules: {
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/indent-binary-ops': ['error', 'tab'],
+			'@stylistic/no-tabs': 'off',
+			'@stylistic/linebreak-style': ['error', 'unix'],
+			'@stylistic/eol-last': ['error', 'never'],
+			'@stylistic/brace-style': ['error', '1tbs'],
+			'@stylistic/arrow-parens': ['error', 'as-needed'],
+		},
+	},
+])
