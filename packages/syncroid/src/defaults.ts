574007a9d551e2516beda4105217fe40aebd8623
kh4f <kh4f.dev@gmail.com>
2025-10-26 13:07:01 +0300

    refactor(defaults): set default `root` to '.' and `dest` to '/storage/emulated/0/Syncroid'

--- packages/syncroid/src/defaults.ts
+++ packages/syncroid/src/defaults.ts
@@ -1,8 +1,8 @@
 import type { ResolvedConfig } from '@/types'
 
 export const defaultConfig: ResolvedConfig = {
-	root: '',
+	root: '.',
 	include: [],
 	exclude: [],
-	dest: '',
+	dest: '/storage/emulated/0/Syncroid',
 }
