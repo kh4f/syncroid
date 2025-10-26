7a636067da193e67e53f720694266341da06251f
kh4f <kh4f.dev@gmail.com>
2025-10-26 11:35:51 +0300

    refactor(syncroid): accept `opts` object instead of destructured params

--- packages/syncroid/src/syncroid.ts
+++ packages/syncroid/src/syncroid.ts
@@ -2,14 +2,14 @@ import { basename, join } from 'node:path/posix'
 import { run, findPaths } from '@/utils'
 import type { SyncroidOptions } from '@/types'
 
-export default function syncroid({ root, include, exclude, dest }: SyncroidOptions) {
-	const entries = findPaths(root, include, exclude)
+export default function syncroid(opts: SyncroidOptions) {
+	const entries = findPaths(opts.root, opts.include, opts.exclude)
 
 	for (const entry of entries) {
 		if (entry.endsWith('/')) {
-			run('adb', ['shell', 'rm', '-rf', join(dest, basename(entry))])
+			run('adb', ['shell', 'rm', '-rf', join(opts.dest, basename(entry))])
 		}
 	}
 
-	run('adb', ['push', ...entries, dest, '--sync'])
+	run('adb', ['push', ...entries, opts.dest, '--sync'])
 }
