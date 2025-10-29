79e33ce829d221bb86bf8fc692eef6e561ce3943
kh4f <kh4f.dev@gmail.com>
2025-10-29 19:58:06 +0300

    style(config-resolver): convert `resolveConfig` to concise arrow function

--- packages/syncroid/src/utils/config-resolver.ts
+++ packages/syncroid/src/utils/config-resolver.ts
@@ -1,11 +1,9 @@
 import type { ResolvedConfig, UserConfig } from '@/types'
 import { defaultConfig } from '@/defaults'
 
-export const resolveConfig = (userConfig: UserConfig): ResolvedConfig => {
-	return {
-		...defaultConfig,
-		...userConfig,
-		include: [...new Set([...defaultConfig.include, ...(userConfig.include ?? [])])],
-		exclude: [...new Set([...defaultConfig.exclude, ...(userConfig.exclude ?? [])])],
-	}
-}
+export const resolveConfig = (userConfig: UserConfig): ResolvedConfig => ({
+	...defaultConfig,
+	...userConfig,
+	include: [...new Set([...defaultConfig.include, ...(userConfig.include ?? [])])],
+	exclude: [...new Set([...defaultConfig.exclude, ...(userConfig.exclude ?? [])])],
+})
