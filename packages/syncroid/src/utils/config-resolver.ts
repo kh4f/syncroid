f6b1c61a9d86d96daf3ab089e459a249f191445a
kh4f <kh4f.dev@gmail.com>
2025-10-29 19:50:04 +0300

    fix(config-resolver): merge `include` and `exclude` arrays instead of overwriting

--- packages/syncroid/src/utils/config-resolver.ts
+++ packages/syncroid/src/utils/config-resolver.ts
@@ -2,5 +2,10 @@ import type { ResolvedConfig, UserConfig } from '@/types'
 import { defaultConfig } from '@/defaults'
 
 export const resolveConfig = (userConfig: UserConfig): ResolvedConfig => {
-	return { ...defaultConfig, ...userConfig }
+	return {
+		...defaultConfig,
+		...userConfig,
+		include: [...new Set([...defaultConfig.include, ...(userConfig.include ?? [])])],
+		exclude: [...new Set([...defaultConfig.exclude, ...(userConfig.exclude ?? [])])],
+	}
 }
