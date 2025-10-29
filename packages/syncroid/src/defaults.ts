c9cb486d5f06597462d940bde9a620004b4b8d80
kh4f <kh4f.dev@gmail.com>
2025-10-29 19:38:58 +0300

    feat(config): add default `exclude` patterns for `node_modules` and `.git`

--- packages/syncroid/src/defaults.ts
+++ packages/syncroid/src/defaults.ts
@@ -3,7 +3,7 @@ import type { ResolvedConfig } from '@/types'
 export const defaultConfig: ResolvedConfig = {
 	root: '.',
 	include: [],
-	exclude: [],
+	exclude: ['**/node_modules', '**/.git'],
 	dest: '/storage/emulated/0/Syncroid',
 	aggregatePaths: false,
 }
